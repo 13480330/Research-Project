0b0dab5b35e5ee9d302dba9f3457b89dcab72283
13480330 <6664978271@qq.com>
2020-10-13 12:32:54 +0800

    new

---
+++ Assets/CarAgent.cs
@@ -0,0 +1,32 @@
+﻿using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class CarAgent : Agent
+{
+    // Start is called before the first frame update
+    Rigidbody R;
+    void Start()
+    {
+        R = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        Vector3 f = Vector3.zero;
+        f.x = vectorAction[0];
+        f.z = vectorAction[1];
+        R.AddForce(f);
+    }
+}
---
+++ Assets/Shader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Shader : MonoBehaviour
+{
+    public Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    // Update is called once per frame
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (mat != null)
+        {
+            Graphics.Blit(source, destination, mat);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
